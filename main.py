f78b7e513941a0882da011162a33b2a820c14eb8
nonsonoalessio <35069388+nonsonoalessio@users.noreply.github.com>
2020-12-04 16:44:43 +0100

    Prima modifica.
    Creazione di un file to do, inizializzazione sorgente e readme.

---
+++ main.py
@@ -0,0 +1,14 @@
+# importo i vari moduli necessari
+import os
+
+# definisco alcune funzioni usate più in avanti
+# prassi di code cleanup
+
+
+# # # # # #      M  A  I  N      # # # # #
+# definisco alcuni valori "notevoli" (aka costanti)
+modalità = 3
+
+# baso il programma su due modalità: 
+# * read only, per interrogare il database;
+# * write through, per salvare dati nel database;
