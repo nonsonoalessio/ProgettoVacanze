51a1018ce0fbdeeec5340c67b33babd56934b1fe
nonsonoalessio <35069388+nonsonoalessio@users.noreply.github.com>
2020-12-07 19:51:56 +0100

    Implementazione di SQLite. Inizializzazione modalità.

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Questo è il repo GitHub su cui è hostato il progetto di **Giura Alessio Donato
 ## Cosa contiene?
 Su questo repo vi sono diversi file, tra cui:
 * un file sorgente Python (`main.py`), attraverso il quale si va a creare un database ed implementarlo in un programmino basilare, eseguibile attraverso un terminale/cmd;
-* un file con estensione **.ini** (`database.ini`), che contiene i settaggi di base sfruttati nel programma per la connessione al server db (N. B.: non è incluso nel file `.gitignore`, quindi clonando il repo non vi sono problemi; sarebbe bene includerlo nel `.gitignore`, ma per questioni di praticità non è necessario, in questo caso);
+* i file di gestione del db; _la lista?_
 * il file **README.md** (che state leggendo già ora), attraverso il quale si illustra, in modo panoramico, tutto l'applicativo (già opportunamente commentato) e si va a fare l'analisi della situazione, con riflessioni e l'illustrazione del metodo logico, a partire dallo _schema ER_;
-* vari file nascosti (file di managment del repo), una task-list (`roadmap.todo`) non necessari all'esecuzione del codice;
+* vari file (tra cui: file di managment del repo, la task-list `roadmap.todo`) non necessari all'esecuzione del codice;
 * eseguibile (`main.exe`), così da non dover creare il pacchetto in un secondo momento.
 
 # Analisi del codice
--- main.py
+++ main.py
@@ -2,50 +2,116 @@
 import os
 import webbrowser
 import time # (uso: time.sleep(value int | float))
+import sqlite3
+from sqlite3 import Error
 
 # definisco alcune funzioni usate più in avanti
 # prassi di code cleanup
-def eccezioneValueError():
-    print('E\' stato inserito un valore non valido che ha causato l\'eccezione ValueError.\nRiavvio il programma...\n')
-    os.system('main.exe')
-    os.system('exit')
+def calcoloCF():
+    print('Funzione vuota')
+def connessione(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print('Who-ops! C\'è stato un incidente.')
+        print(e)
+    return conn
 def scritturaDati():
-    print('Hello world')
+    class cliente:
+        nome = ''
+        cognome = ''
+        giornoNascita = 0
+        meseNascita = 0
+        annoNascita = 0
+        codiceFiscale = ''
+        codiceStanza = 0
+    class stanza:
+        codiceStanza = 0
+        capienza = 0
+        occupata = False
+        occupataDa = ''
+    class prenotazione:
+        codicePrenotazione = ''
+        effettuataDa = ''
+        codiceStanza = 0    
+    print('Questa è la modalità di immissione dati.')
+    time.sleep(1)
+    print('Immettiamo quindi una nuova voce nel nostro database (ulteriori informazioni nella guida)')
+    time.sleep(1)
 def letturaDati():
     print('Hello world')
+def creazioneTabella(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print('Who-ops! C\'è stato un incidente.')
+        print(e)
+def main():
+    database = r"Database\database.db"
+    sql_create_cliente_table = """CREATE TABLE IF NOT EXISTS cliente (
+        codiceFiscale text PRIMARY KEY,
+        nome text NOT NULL,
+        cognome text NOT NULL,
+        giornoNascita integer NOT NULL,
+        meseNascita integer NOT NULL,
+        annoNascita integer NOT NULL,
+        codiceStanza integer NOT NULL,
+    );"""
+    sql_create_stanza_table = """CREATE TABLE IF NOT EXISTS stanza (
+        codiceStanza integer PRIMARY KEY,
+        capienza integer,
+        occupata integer NOT NULL,
+        occupataDa text,
+        FOREIGN KEY (occupataDa) REFERENCES cliente (codiceFiscale) 
+    );"""
+    sql_create_prenotazione_table = """CREATE TABLE IF NOT EXISTS prenotazione (
+        codicePrenotazione text PRIMARY KEY,
+        effettuataDa text NOT NULL,
+        codiceStanza integer NOT NULL,
+        FOREIGN KEY (effettuataDa) REFERENCES cliente (codiceFiscale)
+        FOREIGN KEY (codiceStanza) REFERENCES stanza (codiceStanza)
+    );"""
+    conn = connessione(database)
+    if conn is not None:
+        creazioneTabella(conn, sql_create_cliente_table)
+        creazioneTabella(conn, sql_create_stanza_table)
+        creazioneTabella(conn, sql_create_prenotazione_table)
+    else:
+        print('Who - OPS! Si è verificato un errore')
 
 # # # # # #      M  A  I  N      # # # # #
+# mi collego al db
+if __name__ == '__main__':
+    main()
 # definisco alcuni valori "notevoli" (aka costanti)
 # dichiaro ed inizializzo alcuni valori da usare dopo, anche se non necessario (ma più comodo)
 modalità = 0
 repoLink = 'https://github.com/nonsonoalessio/ProgettoVacanze'
 s = 's'
 n = 'n'
+inserimento = 'inserimento'
+lettura = 'lettura'
+guida = 'guida'
 
 # baso il programma su due modalità: 
 # * read only, per interrogare il database;
-# * write through, per salvare dati nel database;
-modalità = input ('Inserisci il codice della modalità:\n1) inserimento dati\2) lettura dati\n3) apri guida\nScelta: ')
-try:
-    modalità = int(modalità)
-except ValueError:
-    eccezioneValueError()
-while modalità != 1 & modalità != 2 & modalità != 3:
+# * write through, per salvare dati nel database.
+modalità = input ('Inserisci il codice della modalità. Immetti:\n"inserimento" per inserire dati\n"lettura" per leggere i dati\n"guida" per aprire la guida\nScelta: ')
+while modalità != inserimento and modalità != lettura and modalità != guida:
     print('Il codice della scelta non è valido, riprovare per favore.')
-    modalità = input ('Inserisci il codice della modalità:\n1) inserimento dati\2) lettura dati\n3) apri guida\nScelta: ')
-    try:
-        modalità = int(modalità)
-    except ValueError:
-        eccezioneValueError()
-if modalità == 3:
+    modalità = input ('Inserisci il codice della modalità. Immetti:\n"inserimento" per inserire dati\n"lettura" per leggere i dati\n"guida" per aprire la guida\nScelta: ')
+if modalità == guida:
     webbrowser.open(repoLink)
-elif modalità == 1:
+elif modalità == inserimento:
     scritturaDati()
-elif modalità == 2:
+elif modalità == lettura:
     letturaDati()
 
 rilancio = input('Programma in chiusura. Desideri rilanciarlo? Immettere "s" per rilanciare, "n" per mantenere chiuso.\n Valore immesso: ')
-while rilancio != s & rilancio != n:
+while rilancio != s and rilancio != n:
     print('Scelta non valida. Riprovare per favore.')
     rilancio = input('Programma in chiusura. Desideri rilanciarlo? Immettere "s" per rilanciare, "n" per mantenere chiuso.\n Valore immesso: ')
 if rilancio == s:
